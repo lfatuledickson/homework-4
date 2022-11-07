44841018894d8178e2d7354f653a139bade1c61e
lfatuledickson <lfatuledickson@mail.valenciacollege.edu>
2022-11-07 12:38:03 -0500

    Initial Commitment

---
+++ main.js
@@ -0,0 +1,8 @@
+let firstName = "Luis";
+let lastName = "Fatule";
+let age = 11;
+
+fullNameAlert = "My name is " + firstName + " " + lastName + ".";
+console.log("This class is op! I am learning a lot!");
+alert(fullNameAlert);
+alert("I am " + age + " years old.");
